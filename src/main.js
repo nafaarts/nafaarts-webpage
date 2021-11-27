880f3456570bb77779915515d64b48b63edd46be
Nafaarts <71387948+nafaarts@users.noreply.github.com>
2021-11-27 13:24:20 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,8 @@
-let dots = ""
-window.setInterval( function() {
-    if ( dots.length > 3 ) 
-        dots = "";
-    else 
-        dots += "."; 
-  
-    document.querySelector('body').innerHTML = "This page is under maintenance, You will be redirect" + dots
-}, 250);
+import App from "./App.svelte";
 
-window.setTimeout(() => {
-   console.log('Redirected..')
-}, 5000);
+const app = new App({
+    target: document.body,
+});
 
-//import App from "./App.svelte";
-
-// const app = new App({
-//   target: document.body,
-// });
-
-// export default app;
+export default app;
 
